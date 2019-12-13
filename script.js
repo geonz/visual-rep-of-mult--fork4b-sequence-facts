e94f4b177d1cf556d91e5bccc9bb07c4fc5a1313
S Jones <siouxgeonz@gmail.com>
2019-12-13 18:01:46 +0000

    Changed "check" so it's temporarily short. Changed newFact so it changed the index, not the fact.

--- script.js
+++ script.js
@@ -4,6 +4,57 @@ var factNum=5;// the starting number to be multiplied by; the 'answer'
 var factSequence=[2,5,4,9,3,6,7,8];
 var factIndex=0;
 var factSet=factSequence[factIndex];
+
+
+function nextFact()
+{
+    factIndex++;  
+    factSet=factSequence[factIndex];
+  p1.innerHTML="next fact is" + factSet;
+  startUp();
+  // we *will* if this works ... have it go to the next num in the array that is our sequence ***FUTURE THING HERE ***
+  }
+function startOver()
+{
+  factSet=2;
+  startUp();
+}
+function startUp()
+{
+     A1.style.display="none";
+  check2.style.display="none";
+  factNum=1;
+     check1.disabled=false;
+  start.disabled=true;
+  reset.disabled=false;
+     newFact();
+}
+
+function newFact()
+{if (factNum>10)
+{
+  p1.innerHTML= "you're done!" +'<button id="check1" class="button1" onclick="nextFact();">Next Fact</button>'
+;
+}
+// maybe that should be in "check answer" or in both. 
+else 
+  {p1.innerHTML="fact number  is: " + factNum;
+   
+  feedback.innerHTML="The picture represents  ";
+  factor2.innerHTML= factSet + " x " + '<input type="number" id="factor" size="1" maxlength="2"/>';
+   A1.style.display="none";
+  check2.style.display="none";
+   A1.innerHTML=  factSet + " x " + factNum + " =" + ' <input type="number" id="multAnswer"  size="1" maxlength="2"/>';
+ check1.disabled=false;
+}
+//changeBackgrounds('section > div', 'palevioletred');
+
+changeMultiples('section > div','palevioletred','yellow');
+}
+
+
+
+
 function createMultImage()
 {
   let multPicHTML="";
@@ -65,51 +116,6 @@ else
   feedback.innerHTML="Check your entry; it doesn't seem to be right. I'm just a computer, though..."
 }
 }
-function nextFact()
-{
-    factSet++;  
-  p1.innerHTML="next fact is" + factSet;
-  startUp();
-  // we *will* if this works ... have it go to the next num in the array that is our sequence ***FUTURE THING HERE ***
-  }
-function startOver()
-{
-  factSet=2;
-  startUp();
-}
-function startUp()
-{
-     A1.style.display="none";
-  check2.style.display="none";
-  factNum=1;
-     check1.disabled=false;
-  start.disabled=true;
-  reset.disabled=false;
-     newFact();
-}
-
-function newFact()
-{if (factNum>10)
-{
-  p1.innerHTML= "you're done!" +'<button id="check1" class="button1" onclick="nextFact();">Next Fact</button>'
-;
-}
-// maybe that should be in "check answer" or in both. 
-else 
-  {p1.innerHTML="fact number  is: " + factNum;
-   
-  feedback.innerHTML="The picture represents  ";
-  factor2.innerHTML= factSet + " x " + '<input type="number" id="factor" size="1" maxlength="2"/>';
-   A1.style.display="none";
-  check2.style.display="none";
-   A1.innerHTML=  factSet + " x " + factNum + " =" + ' <input type="number" id="multAnswer"  size="1" maxlength="2"/>';
- check1.disabled=false;
-}
-//changeBackgrounds('section > div', 'palevioletred');
-
-changeMultiples('section > div','palevioletred','yellow');
-}
-
 
 
 
@@ -122,7 +128,7 @@ function checkAnswer()
   {
     feedback.innerHTML="That's right!";
     // need a "next fact" or 
-    factNum++;
+    factNum+=9;
 newFact();
     // go to the next one.   
   }
